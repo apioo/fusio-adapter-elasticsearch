2b9a0c2d377910cabcd1653cb51887903f78cf22
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-07-18 21:32:29 +0200

    use port 9200

--- tests/Connection/ElasticsearchTest.php
+++ tests/Connection/ElasticsearchTest.php
@@ -44,7 +44,7 @@ class ElasticsearchTest extends TestCase
         $connectionFactory = $this->getConnectionFactory()->factory(Elasticsearch::class);
 
         $config = new Parameters([
-            'host' => ['127.0.0.1'],
+            'host' => ['127.0.0.1:9200'],
         ]);
 
         $connection = $connectionFactory->getConnection($config);
@@ -58,7 +58,7 @@ class ElasticsearchTest extends TestCase
         $connectionFactory = $this->getConnectionFactory()->factory(Elasticsearch::class);
 
         $config = new Parameters([
-            'host' => ['127.0.0.1'],
+            'host' => ['127.0.0.1:9200'],
         ]);
 
         $connection = $connectionFactory->getConnection($config);
